223739f1c5f80c65da8d506cc5617cddb0d8b99a
kayla tapiador <kaylatapiador@yahoo.com>
2024-04-04 22:07:09 -0700

    api

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const app = express()
 
+require('dotenv').config()
+const PORT = process.env.PORT
+app.use(express.json())
+app.use(cors())
+
+// MONGOOSE
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
+    console.log('connected to mongo on: ', process.env.MONGO_URI)
+})
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+const booksController = require('./controller/books_controller')
+app.use('/books', booksController)
+
+app.listen(PORT, () => {
+    console.log(`Listening on port: ${PORT}`)
+})
